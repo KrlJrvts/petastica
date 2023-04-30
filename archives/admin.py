f429eac1b3781a9cf83f809ab78d6ca29ddcb073
Kaarel Järveots <115338028+KrlJrvts@users.noreply.github.com>
2023-04-30 16:04:09 +0300

    Database init

--- archives/admin.py
+++ archives/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from archives.models import Animal
+
 
 # Register your models here.
+
+class AnimalAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Animal, AnimalAdmin)
--- archives/models.py
+++ archives/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Animal(models.Model):
+    animal_name = models.CharField(blank=False, null=False, max_length=128)
+    animal_description = models.CharField(blank=False, null=False, max_length=2000)
+    animal_weight = models.CharField(blank=False, null=False, max_length=128)
+    animal_length = models.CharField(blank=False, null=False, max_length=128)
+    animal_habitat = models.CharField(blank=False, null=False, max_length=128)
+    animal_fact1 = models.CharField(blank=True, null=True, max_length=256)
+    animal_fact2 = models.CharField(blank=True, null=True, max_length=256)
+    animal_fact3 = models.CharField(blank=True, null=True, max_length=256)
+    animal_sound = models.CharField(blank=True, null=True, max_length=128)
+    animal_image1 = models.CharField(blank=True, null=True, max_length=128)
+    animal_image2 = models.CharField(blank=True, null=True, max_length=128)
