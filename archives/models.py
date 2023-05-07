6a6366219707e8ba30fa88c4f27473c69f42b6c1
Kaarel Järveots <115338028+KrlJrvts@users.noreply.github.com>
2023-05-07 14:18:31 +0300

    Database init

--- archives/models.py
+++ archives/models.py
@@ -5,25 +5,28 @@ from django.db import models
 
 
 class Animal(models.Model):
+    SHORT_TXT = 128
+    TXT = 256
+    
     animal_name = models.CharField(
         blank=False,
         null=False,
-        max_length=128
+        max_length=SHORT_TXT
     )
-    animal_description = models.CharField(
+    animal_description = models.TextField(
         blank=False,
         null=False,
-        max_length=2000
+        max_length=2560
     )
     animal_weight = models.CharField(
         blank=False,
         null=False,
-        max_length=128
+        max_length=SHORT_TXT
     )
-    animal_length = models.CharField(blank=False, null=False, max_length=128)
-    animal_habitat = models.CharField(blank=False, null=False, max_length=128)
-    animal_fact1 = models.CharField(blank=True, null=True, max_length=256)
-    animal_fact2 = models.CharField(blank=True, null=True, max_length=256)
-    animal_fact3 = models.CharField(blank=True, null=True, max_length=256)
-    animal_sound = models.CharField(blank=True, null=True, max_length=128)
+    animal_length = models.CharField(blank=False, null=False, max_length=SHORT_TXT)
+    animal_habitat = models.CharField(blank=False, null=False, max_length=SHORT_TXT)
+    animal_fact1 = models.CharField(blank=True, null=True, max_length=TXT)
+    animal_fact2 = models.CharField(blank=True, null=True, max_length=TXT)
+    animal_fact3 = models.CharField(blank=True, null=True, max_length=TXT)
+    animal_sound = models.CharField(blank=True, null=True, max_length=SHORT_TXT)
     animal_image1 = models.ImageField(blank=True, null=True, upload_to='')
